9e8278486654835741868c04f756ffc5a10deb6f
fe-lipejs <felipejsf7@gmail.com>
2026-08-20 14:28:28 -0300

    feat: setup MPA for /playlist to have custom Open Graph meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { resolve } from 'path';
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -14,5 +16,13 @@ export default defineConfig(({ mode }) => {
     server: {
       allowedHosts,
     },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          playlist: resolve(__dirname, 'playlist/index.html')
+        }
+      }
+    }
   };
 });
